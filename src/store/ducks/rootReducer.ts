76f10cb77a2594f16849e9bac4f920f4e0af6b44
DestroyeerU <destroyeeru18@gmail.com>
2020-04-18 21:51:32 -0300

    Project structure added

---
+++ src/store/ducks/rootReducer.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// import foo from './foo/reducers';
+
+const reducers = combineReducers({
+  // foo,
+});
+
+export default reducers;
---
+++ src/store/ducks/rootSagas.ts
@@ -0,0 +1,9 @@
+import { all } from 'redux-saga/effects';
+
+// import foo from './foo/sagas';
+
+export default function* rootSaga() {
+  yield all([
+    // foo,
+  ]);
+}
