53a0627bfebf01e8492956034b0af6bc1a28977f
DestroyeerU <destroyeeru18@gmail.com>
2020-08-28 12:59:07 -0300

    eslint improved

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const Main = () => {
+const Main: React.FC = ({ children }) => {
   return (
     <Container>
       <h1>Main</h1>
